71f38d507d075070e89cb9d248c11023185832e9
Wojciech Szymczak <sersicoh@gmail.com>
2022-09-02 20:09:52 +0200

    first Route to App.js > MovieList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { HashRouter } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import NavigationBar from "./common/Navigation";
+import MovieList from "./features/Movieslist";
 
 function App() {
   return (
     <HashRouter>
       <nav>
         <NavigationBar />
+        <Switch>
+          <Route path="/movies">
+            <MovieList />
+          </Route>
+        </Switch>
       </nav>
     </HashRouter>
   );
