d20541d229ed05f79243ed1677fdb8da36d1b6e3
Wojciech Szymczak <sersicoh@gmail.com>
2022-09-03 00:12:43 +0200

    added description Info

--- src/common/TilesMovie/TileMovie/styled.js
+++ src/common/TilesMovie/TileMovie/styled.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
-export const StyledTileMovie = styled.div`
+export const StyledTileMovie = styled.section`
 
+    position: relative;
     width: 324px;
     height: 650px;
     flex: none;
@@ -17,8 +18,19 @@ export const StyledTileMovie = styled.div`
         height: 201px;
     }
 `;
+export const MovieSection = styled.div`
+    display: grid;
+    grid-template-rows: auto auto;
 
-export const StyledPoster = styled.img`
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        grid-template-columns: auto 1fr;
+    }
+`;
+export const Poster = styled.img`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 16px;
     width: 292px;
     height: 434px;
     border-radius: 5px;
@@ -30,8 +42,7 @@ export const StyledPoster = styled.img`
         top: 0px;
     }
 `;
-
-export const StyledNoPoster = styled.div`
+export const NoPoster = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -48,7 +59,6 @@ export const StyledNoPoster = styled.div`
         top: 0px;
     }
 `;
-
 export const StyledMovieIcon = styled.div`
     display: flex;
     justify-content: center;
@@ -59,27 +69,102 @@ export const StyledMovieIcon = styled.div`
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
         width: 48px;
         height: 48px;
-
     }
 `;
+export const Destription = styled.section`
 
-export const StyledDestription = styled.section`
-    margin: 16px;
+    margin: 0px 0px 0px 16px;
+    
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        margin: 16px 0px 0px 0px;
+    }
 `;
-
-export const StyledMovieTitle = styled.div`
+export const MovieTitle = styled.div`
 
     font-weight: ${({ theme }) => theme.fontSize.medium};
     font-size: 22px;
     display: flex;
     align-items: center;
-`;
 
-export const StyledMovieYear = styled.div`
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        font-size: 16px;
+    }
+`;
+export const MovieYear = styled.div`
 
     font-weight: ${({ theme }) => theme.fontSize.regular};
     color: ${({ theme }) => theme.colors.darkerGrey};
     font-size: 22px;
     display: flex;
     align-items: center;
+    padding: 8px 0px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        font-size: 13px;
+    }
+`;
+export const Tags = styled.section`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    flex-wrap: wrap;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        gap: 8px;
+        align-items: flex-start;     
+    }
+`;
+export const Tag = styled.div`
+    font-size: 14px;
+    font-weight: ${({ theme }) => theme.fontSize.regular};
+    padding: 8px 16px;
+    border-radius: 5px;
+    background: ${({ theme }) => theme.colors.mystic};
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        font-size: 10px;
+        padding: 4px 8px;
+    }
+`;
+export const RatingSection = styled.section`
+
+    display: flex;
+    flex-direction: row;
+    align-items: flex-end;
+    position: absolute;
+    bottom: 16px;
+    gap: 12px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        position: relative;
+        bottom: -12px;
+        gap: 8px;
+    }
+`;
+export const StyledStarIcon = styled.div`
+    width: 24px;
+    height: 23px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        width: 16px;
+        height: 15px;
+    }
+`;
+export const Rate = styled.div`
+    font-size: 16px;
+    font-weight: ${({ theme }) => theme.fontSize.bold};
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        font-size: 13px;
+    }
+    
+`;
+export const Votes = styled.div`
+    font-size: 16px;
+    font-weight: ${({ theme }) => theme.fontSize.regular};
+    color: ${({ theme }) => theme.colors.darkerGrey};
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
+        font-size: 13px;
+    }
 `;
