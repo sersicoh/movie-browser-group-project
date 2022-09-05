5ef3b6ecac35b1b7790d8fea2da944ab54c358d0
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-09-05 13:36:28 +0200

    the same : ifMovieLoading, ifPeopleLoading

--- src/features/getMovieData/Test/test.js
+++ src/features/getMovieData/Test/test.js
@@ -7,16 +7,16 @@ import {selectMovies, fetchPopularMovies} from "../MovieList/movieSlice";
 
 
 const Test = () => {
-  const {setPeopleLoading} = useSelector(selectPeople);
-  const {ifLoading} = useSelector(selectMovies);
+  const {ifPeopleLoading} = useSelector(selectPeople);
+  const {ifMoviesLoading} = useSelector(selectMovies);
   const dispatch = useDispatch();
 
 return (
   <Navigation>
-      <Button onClick={() => dispatch(fetchPopularMovies())} disabled={ifLoading}>
+      <Button onClick={() => dispatch(fetchPopularMovies())} disabled={ifMoviesLoading}>
         FILMY
       </Button>
-      <Button onClick={() => dispatch(fetchPopularPeople())} disabled={setPeopleLoading}>
+      <Button onClick={() => dispatch(fetchPopularPeople())} disabled={ifPeopleLoading}>
         CZŁOWIEKI
       </Button>
   </Navigation>
