81e35142420778d4de62c512533934ddd3761c00
Wojciech Szymczak <sersicoh@gmail.com>
2022-09-05 19:16:09 +0200

    fix styles to responsive

--- src/common/TileMovie/index.js
+++ src/common/TileMovie/index.js
@@ -1,14 +1,28 @@
 import MovieIcon from '../../iconComponents/MovieIcon'
 import StarIcon from '../../iconComponents/StarIcon'
-import { Destription, StyledMovieIcon, MovieTitle, MovieYear, NoPoster, Poster, StyledTileMovie, MovieSection, Tag, Tags, StyledStarIcon, RatingSection, Rate, Votes } from "./styled";
+import {
+    StyledMovieIcon,
+    MovieTitle,
+    MovieYear,
+    NoPoster,
+    Poster,
+    StyledTileMovie,
+    Wrapper,
+    Tag,
+    Tags,
+    StyledStarIcon,
+    RatingSection,
+    Rate,
+    Votes
+} from "./styled";
 
 const TileMovie = ({ movie }) => {
 
     const picture = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-
+    
     return (
-        <StyledTileMovie>
-            <MovieSection>
+        <Wrapper>
+            <StyledTileMovie>
                 {picture
                     ?
                     <Poster src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
@@ -18,22 +32,20 @@ const TileMovie = ({ movie }) => {
                             <MovieIcon width={"100%"} height={"100%"} />
                         </StyledMovieIcon>
                     </NoPoster>}
-                <Destription>
-                    <MovieTitle>{movie.title}</MovieTitle>
-                    <MovieYear>{movie.release_date}</MovieYear>
-                    <Tags>
-                        <Tag>{movie.genre_ids[0]}</Tag>
-                    </Tags>
-                    <RatingSection>
-                        <StyledStarIcon>
-                            <StarIcon width={"100%"} height={"100%"} />
-                        </StyledStarIcon>
-                        <Rate>{movie.vote_average}</Rate>
-                        <Votes>{movie.vote_count} votes</Votes>
-                    </RatingSection>
-                </Destription>
-            </MovieSection>
-        </StyledTileMovie>
+                <MovieTitle>{movie.title}</MovieTitle>
+                <MovieYear>{movie.release_date}</MovieYear>
+                <Tags>
+                    <Tag>{movie.genre_ids[0]}</Tag>
+                </Tags>
+                <RatingSection>
+                    <StyledStarIcon>
+                        <StarIcon width={"100%"} height={"100%"} />
+                    </StyledStarIcon>
+                    <Rate>{movie.vote_average}</Rate>
+                    <Votes>{movie.vote_count} votes</Votes>
+                </RatingSection>
+            </StyledTileMovie>
+        </Wrapper>
     );
 
 };
