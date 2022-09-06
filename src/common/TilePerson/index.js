1ce9f914be74360d23dc0c1418e704ace668e063
Wojciech Szymczak <sersicoh@gmail.com>
2022-09-06 15:13:03 +0200

    refactor styles for PersonTile to responsive

--- src/common/TilePerson/index.js
+++ src/common/TilePerson/index.js
@@ -1,28 +1,28 @@
 import PersonIcon from "../../iconComponents/PersonIcon";
-import { Destription, PersonName, NoPoster, Poster, StyledTilePerson, PersonSection, StyledPersonIcon } from "./styled";
+import { Destription, PersonName, NoPoster, Poster, StyledTilePerson, StyledPersonIcon, Wrapper } from "./styled";
 
 const TilePerson = ({ person }) => {
 
     const picture = `https://image.tmdb.org/t/p/w500/${person.profile_path}`;
 
     return (
-        <StyledTilePerson>
-            <PersonSection>
-                {picture
-                    ?
-                    <Poster src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`} />
-                    :
-                    <NoPoster>
-                        <StyledPersonIcon>
-                            <PersonIcon width={"100%"} height={"100%"} />
-                        </StyledPersonIcon>
-                    </NoPoster>
+        <Wrapper>
+            <StyledTilePerson>
+                    {picture
+                        ?
+                        <Poster src={`https://image.tmdb.org/t/p/w500/${person.profile_path}`} />
+                        :
+                        <NoPoster>
+                            <StyledPersonIcon>
+                                <PersonIcon width={"100%"} height={"100%"} />
+                            </StyledPersonIcon>
+                        </NoPoster>
                     }
-                <Destription>
-                    <PersonName>{person.name}</PersonName>
-                </Destription>
-            </PersonSection>
-        </StyledTilePerson>
+                    <Destription>
+                        <PersonName>{person.name}</PersonName>
+                    </Destription>
+            </StyledTilePerson>
+        </Wrapper>
     );
 };
 
