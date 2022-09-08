706327df203b488041312464ecb455b790f49185
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-09-08 17:43:35 +0200

    map added into genre tags

--- src/common/TileMovie/index.js
+++ src/common/TileMovie/index.js
@@ -1,41 +1,64 @@
-import MovieIcon from '../../iconComponents/MovieIcon'
-import StarIcon from '../../iconComponents/StarIcon'
-import { Destription, StyledMovieIcon, MovieTitle, MovieYear, NoPoster, Poster, StyledTileMovie, MovieSection, Tag, Tags, StyledStarIcon, RatingSection, Rate, Votes } from "./styled";
+import MovieIcon from "../../iconComponents/MovieIcon";
+import StarIcon from "../../iconComponents/StarIcon";
+import {
+  Destription,
+  StyledMovieIcon,
+  MovieTitle,
+  MovieYear,
+  NoPoster,
+  Poster,
+  StyledTileMovie,
+  MovieSection,
+  Tag,
+  Tags,
+  StyledStarIcon,
+  RatingSection,
+  Rate,
+  Votes,
+} from "./styled";
+import { useSelector } from "react-redux";
+import { selectGenres } from "../../features/getMovieData/MovieList/movieSlice";
 
-const TileMovie = ({ movie }) => {
+const TileMovie = ({ movie, genreIds }) => {
+  const picture = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  const { genreList } = useSelector(selectGenres);
 
-    const picture = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
-
-    return (
-        <StyledTileMovie>
-            <MovieSection>
-                {picture
-                    ?
-                    <Poster src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-                    :
-                    <NoPoster>
-                        <StyledMovieIcon>
-                            <MovieIcon width={"100%"} height={"100%"} />
-                        </StyledMovieIcon>
-                    </NoPoster>}
-                <Destription>
-                    <MovieTitle>{movie.title}</MovieTitle>
-                    <MovieYear>{movie.release_date}</MovieYear>
-                    <Tags>
-                        <Tag>{movie.genre_ids[0]}</Tag>
-                    </Tags>
-                    <RatingSection>
-                        <StyledStarIcon>
-                            <StarIcon width={"100%"} height={"100%"} />
-                        </StyledStarIcon>
-                        <Rate>{movie.vote_average}</Rate>
-                        <Votes>{movie.vote_count} votes</Votes>
-                    </RatingSection>
-                </Destription>
-            </MovieSection>
-        </StyledTileMovie>
-    );
-
+  return (
+    <StyledTileMovie>
+      <MovieSection>
+        {picture ? (
+          <Poster
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+          />
+        ) : (
+          <NoPoster>
+            <StyledMovieIcon>
+              <MovieIcon width={"100%"} height={"100%"} />
+            </StyledMovieIcon>
+          </NoPoster>
+        )}
+        <Destription>
+          <MovieTitle>{movie.title}</MovieTitle>
+          <MovieYear>{movie.release_date}</MovieYear>
+          <Tags>
+            {genreList.map(
+              (genre) =>
+                genreIds.includes(genre.id) && (
+                  <Tag key={genre.id}> {genre.name}</Tag>
+                )
+            )}
+          </Tags>
+          <RatingSection>
+            <StyledStarIcon>
+              <StarIcon width={"100%"} height={"100%"} />
+            </StyledStarIcon>
+            <Rate>{movie.vote_average}</Rate>
+            <Votes>{movie.vote_count} votes</Votes>
+          </RatingSection>
+        </Destription>
+      </MovieSection>
+    </StyledTileMovie>
+  );
 };
 
 export default TileMovie;
