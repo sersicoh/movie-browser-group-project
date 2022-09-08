914c73c5f57a974f91eda9ddd44ff2837372bf6f
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-09-08 14:52:02 +0200

    genreList creation in redux slice

--- src/features/getMovieData/MovieList/movieSlice.js
+++ src/features/getMovieData/MovieList/movieSlice.js
@@ -4,7 +4,7 @@ const moviesSlice = createSlice({
   name: "movieList",
   initialState: {
     movieList: [],
-    movieGenres: [],
+    genreList: [],
     ifMoviesLoading: false,
   },
 
@@ -12,24 +12,34 @@ const moviesSlice = createSlice({
     fetchPopularMovies: (state) => {
       state.ifMoviesLoading = true;
     },
+    fetchGenres: (state) => {
+      state.ifMoviesLoading = true;
+    },
     setMovieList: (state, { payload: movies }) => {
       state.movieList = movies.results;
       state.ifMoviesLoading = false;
+      console.log(state.movieList);
     },
     setLoading: (state) => {
       state.ifMoviesLoading = false;
     },
-    setGenreList: (state, { payload: genre_ids }) => {
-      state.movieList = genre_ids.results;
-      state.ifMoviesLoading = false;
+    setGenreList: (state, { payload: genres }) => {
+      state.genreList = genres.genres;
+      console.log(state.genreList);
     },
   },
 });
 
-export const { fetchPopularMovies, setMovieList, setLoading } =
-  moviesSlice.actions;
+export const {
+  fetchPopularMovies,
+  fetchGenres,
+  setMovieList,
+  setLoading,
+  setGenreList,
+} = moviesSlice.actions;
 
 export const selectMovies = (state) => state.movieList;
+export const selectGenres = (state) => state.genreList;
 export const getMovieById = (state, movieId) =>
   selectMovies(state).movieList.find(({ id }) => id === movieId);
 export const selectMoviesByQuery = (state, query) => {
