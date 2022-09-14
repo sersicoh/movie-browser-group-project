2db664b9b65bd9ad3b2739905cecafcb37610038
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-09-15 01:58:04 +0200

    green are must be added into saga somehow

--- src/features/getMovieData/movieDetailsSaga.js
+++ src/features/getMovieData/movieDetailsSaga.js
@@ -1,10 +1,16 @@
 import { call, delay, put, takeLatest } from "redux-saga/effects";
 import { getMoviesDetails } from "./getData";
 import { fetchMovieDetails, setLoading, setMovieDetails } from "./MovieDetails/movieDetailsSlice";
+//import { useSelector } from "react-redux";
+import { selectGenres } from "../../features/getMovieData/MovieList/movieSlice";
+//import { useParams } from "react-router-dom";
+//import { getMovieById } from "../../features/getMovieData/MovieDetails/movieDetailsSlice";
 
 export function* fetchMovieDetailsWorker() {
   try {
     yield delay(100);
+    //const params = useParams();
+    //const movieId = useSelector((state) => getMovieById(state, params.id));
     const movieId = "610150";
     const movieDetails = yield call(getMoviesDetails, movieId);
     console.log(movieDetails);
--- src/features/movieDetails/PosterBig/index.js
+++ src/features/movieDetails/PosterBig/index.js
@@ -1,4 +1,6 @@
 import StarIcon from "../../../iconComponents/StarIcon";
+import Picture from "../../../iconComponents/PosterBig.png";
+
 import {
   MovieTitle,
   BackgroundColor,
@@ -13,23 +15,24 @@ import {
 } from "./styled";
 
 const PosterBig = () => {
+  const picture = Picture;
+
   return (
     <BackgroundColor>
-      <BackgroundImage>
-        <Wrapper>
-          <InfoSection>
-            <MovieTitle>Full movie title</MovieTitle>
-            <RatingSection>
-              <StyledStarIcon>
-                <StarIcon width={"100%"} height={"100%"} />
-              </StyledStarIcon>
-              <Rate>7.8</Rate>
-              <RateScale> / 10</RateScale>
-            </RatingSection>
-            <Votes>1723 votes</Votes>
-          </InfoSection>
-        </Wrapper>
-      </BackgroundImage>
+      <Wrapper>
+        <BackgroundImage src={picture} />
+        <InfoSection>
+          <MovieTitle>Full movie title</MovieTitle>
+          <RatingSection>
+            <StyledStarIcon>
+              <StarIcon width={"100%"} height={"100%"} />
+            </StyledStarIcon>
+            <Rate>7.8</Rate>
+            <RateScale> / 10</RateScale>
+          </RatingSection>
+          <Votes>1723 votes</Votes>
+        </InfoSection>
+      </Wrapper>
     </BackgroundColor>
   );
 };
