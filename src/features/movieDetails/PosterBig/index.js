04f58773e6cf1b1615116cd6ec83d577da66e28b
Wojciech Szymczak <sersicoh@gmail.com>
2022-09-17 15:18:40 +0200

    refactor to relative

--- src/features/movieDetails/PosterBig/index.js
+++ src/features/movieDetails/PosterBig/index.js
@@ -14,23 +14,24 @@ import {
   Wrapper,
 } from "./styled";
 
-const PosterBig = () => {
-  const picture = Picture;
+const PosterBig = ({ selectedMovie }) => {
+
+  const picture = `https://image.tmdb.org/t/p/w500/${selectedMovie.backdrop_path}`;
 
   return (
     <BackgroundColor>
       <Wrapper>
         <BackgroundImage src={picture} />
         <InfoSection>
-          <MovieTitle>Full movie title</MovieTitle>
+          <MovieTitle>{selectedMovie.title}</MovieTitle>
           <RatingSection>
             <StyledStarIcon>
               <StarIcon width={"100%"} height={"100%"} />
             </StyledStarIcon>
-            <Rate>7.8</Rate>
+            <Rate>{selectedMovie.vote_average}</Rate>
             <RateScale> / 10</RateScale>
           </RatingSection>
-          <Votes>1723 votes</Votes>
+          <Votes>{selectedMovie.vote_count} votes</Votes>
         </InfoSection>
       </Wrapper>
     </BackgroundColor>
