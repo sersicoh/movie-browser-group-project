6f49f9b1edb669ac4035fc90d6c514283d63e567
Wojciech Szymczak <sersicoh@gmail.com>
2022-09-17 15:17:37 +0200

    change type loading

--- src/features/getMovieData/MovieDetails/movieDetailsSlice.js
+++ src/features/getMovieData/MovieDetails/movieDetailsSlice.js
@@ -4,22 +4,24 @@ const moviesDetailsSlice = createSlice({
   name: "movieDetails",
   initialState: {
     movieDetails: [],
-    ifMovieDetailsLoading: false,
+    ifMovieDetailsLoading: "",
   },
   reducers: {
     setLoading: (state) => {
-      state.ifMovieDetailsLoading = true;
-      console.log("click");
+      state.ifMovieDetailsLoading = "loading";
     },
     setMovieDetails: (state, { payload: movieDetails }) => {
       state.movieDetails = movieDetails;
-      state.ifMovieDetailsLoading = false;
+      // poniższy console.log to wyrzucenia gdy nie bedzie potrzebny
+      console.log(movieDetails);
+      state.ifMovieDetailsLoading = "success";
     },
   },
 });
 
 export const { setLoading, fetchMovieDetails, setMovieDetails } = moviesDetailsSlice.actions;
 export const selectMoviesDetails = (state) => state.movieDetails;
+export const selectLoadingState = (state) => state.ifMovieDetailsLoading;
 export const getMovieById = (state, movieId) =>
   selectMoviesDetails(state).movieDetails.find(({ id }) => id === movieId);
 export default moviesDetailsSlice.reducer;
