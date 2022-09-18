49b219c781c85a30b947df6dda5a4b8b2ea5a73d
Szymon Lesko <leskoszymon30@gamil.com>
2022-09-18 12:57:53 +0200

    Created error page

---
+++ src/common/Error/index.js
@@ -0,0 +1,12 @@
+import { ErrorContainer, DangerIcon, Title, Text, Button } from "./styled";
+
+export const ErrorPage = () => {
+    return (
+        <ErrorContainer>
+            <DangerIcon />
+            <Title>Ooops! Something went wrong...</Title>
+            <Text>Please check your network connection<br /> and try again</Text>
+            <Button big to="/movies">Back to home page</Button>
+        </ErrorContainer>
+    )
+};
---
+++ src/common/Error/styled.js
@@ -0,0 +1,74 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { ReactComponent as DangerIcon } from "../../iconComponents/danger.svg"
+import { theme } from "../../theme"
+
+export const ErrorContainer = styled.div`
+margin: 195px auto;
+display: flex;
+flex-direction: column;
+align-items: center;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    margin-top: 120px;
+}
+`;
+
+export const DangerIcon = styled(DangerIcon)`
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    width: 80px;
+}
+`;
+
+export const Title = styled.h2`
+text-align: center;
+font-weight: 600;
+font-size: 36px;
+line-height: 120%;
+margin: 38px 0 0;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 18px;
+    margin-top: 0px;
+}
+`;
+
+export const Text = styled.p`
+margin: 24px;
+text-align: center;
+font-size: 22px;
+font-weight: 500;
+line-height: 130%;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 12px;
+    margin: 15px;
+}
+`;
+
+export const Button = styled(Link)`
+padding: 16px 24px;
+border: none;
+border-radius: 5px;
+background: blue;
+color: white;
+font-weight: 700;
+font-size: 14px;
+line-height: 19px;
+transition: 0.4s;
+text-decoration: none;
+
+@media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 10px;
+    padding: 12px 20px;
+    line-height: 15px;
+}
+
+&:hover{
+    filter: brightness(115%);
+}
+
+&:active{
+    filter: brightness(150%);
+}
+`
