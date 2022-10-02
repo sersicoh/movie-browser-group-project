3b15c83f5441c84817570f09cd8de234e46ad3ff
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-02 18:59:29 +0200

    new version of router used into app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter, Route, Switch, Routes, Navigate, Redirect } from "react-router-dom";
 import NavigationBar from "./common/Navigation";
 import MovieList from "./features/Movieslist";
 import PersonList from "./features/PersonList";
@@ -7,45 +7,47 @@ import PersonDetails from "./features/personDetails";
 
 function App() {
   return (
-    <HashRouter>
-      <nav>
-        <NavigationBar />
-        <Switch>
-          <Route path="/movies">
-            <MovieList />
-          </Route>
-          <Route path="/people">
-            <PersonList />
-          </Route>
-          <Route path="/movieDetails/:id">
-            <MovieDetails />
-          </Route>
-          <Route path="/personDetails/:id">
-            <PersonDetails />
-          </Route>
-          <Route path="/">
-            <Redirect to="/movies" />
-          </Route>
-        </Switch>
-      </nav>
-    </HashRouter>
+ <HashRouter>
+<nav>
+  <NavigationBar />
+  <Routes>
+  <Route path="/" element={<Navigate to="movies"/>}/>
+    <Route path="/movies" element={<Navigate to="1"/>}/>
+    <Route path="/people" element={<Navigate to="1"/>}/>
+    <Route path="/movies/:page" element={<MovieList/>}/>
+    <Route path="/people/:page" element={<PersonList/>}/>
+    <Route path="/movieDetails/:id" element={ <MovieDetails />}/>
+    <Route path="/personDetails/:id" element={<PersonDetails />}/>
+  </Routes>    
+</nav>              
+</HashRouter>
   );
 }
 
 export default App;
 
 
+
+
 {/* <HashRouter>
 <nav>
   <NavigationBar />
-  <Routes>
-  <Route path="/" element={<Navigate to="movies"/>}/>
-    <Route path="/movies" element={<Navigate to="page=1"/>}/>
-    <Route path="/people" element={<Navigate to="page=1"/>}/>
-    <Route path="/movies/page=:page" element={<MovieList/>}/>
-    <Route path="/people/page=:page" element={<PersonList/>}/>
-    <Route path="/movieDetails/:id" element={ <MovieDetails />}/>
-    <Route path="/personDetails/:id" element={<PersonDetails />}/>
-  </Routes>
-</nav> 
-</HashRouter>*/}
+  <Switch>
+    <Route path="/movies">
+      <MovieList />
+    </Route>
+    <Route path="/people">
+      <PersonList />
+    </Route>
+    <Route path="/movieDetails/:id">
+      <MovieDetails />
+    </Route>
+    <Route path="/personDetails/:id">
+      <PersonDetails />
+    </Route>
+    <Route path="/">
+      <Redirect to="/movies" />
+    </Route>
+  </Switch>
+</nav>
+</HashRouter> */}
