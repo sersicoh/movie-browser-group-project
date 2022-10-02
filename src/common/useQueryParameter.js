8b1fd7f88804e059bc1d7f0923ae4fabb7098e87
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-02 19:00:10 +0200

    queryParameter files added to experiment

---
+++ src/common/useQueryParameter.js
@@ -0,0 +1,9 @@
+import { useLocation } from "react-router-dom";
+
+const useQueryParameter = (paramName) => {
+const location = useLocation();
+return (new URLSearchParams(location.search)).get(paramName);
+}
+
+export default useQueryParameter;
+
