e8d3e3bee943c27ad8e7cfc64b7c3cfcb588d140
Jakub Nowakowski <jacob.69.isaac@gmail.com>
2022-10-09 13:28:08 +0200

    DangerIcon position and size optimized for mobile

--- src/common/Error/styled.js
+++ src/common/Error/styled.js
@@ -1,74 +1,73 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { theme } from "../../theme"
+import DangerIcon from "../../iconComponents/DangerIcon";
 
 export const ErrorContainer = styled.div`
-    margin: 195px auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        margin-top: 120px;
-    }
+  margin: 195px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    margin-top: 120px;
+    min-width: 315px;
+  }
 `;
 
-export const StyledDangerIcon = styled.div`
-
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        width: 80px;
-    }
+export const StyledDangerIcon = styled(DangerIcon)`
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    width: 100px;
+    height: 100px;
+  }
 `;
-
 export const Title = styled.h2`
-    text-align: center;
-    font-weight: 600;
-    font-size: 36px;
-    line-height: 120%;
-    margin: 38px 0 0;
+  text-align: center;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+  margin: 38px 0 0;
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        font-size: 18px;
-        margin-top: 0px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 18px;
+    margin-top: 0px;
+  }
 `;
 
 export const Text = styled.p`
-    margin: 24px;
-    text-align: center;
-    font-size: 22px;
-    font-weight: 500;
-    line-height: 130%;
+  margin: 24px;
+  text-align: center;
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 130%;
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        font-size: 12px;
-        margin: 15px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 12px;
+    margin: 15px;
+  }
 `;
 
 export const Button = styled(Link)`
-    padding: 16px 24px;
-    border: none;
-    border-radius: 5px;
-    background: blue;
-    color: white;
-    font-weight: 700;
-    font-size: 14px;
-    line-height: 19px;
-    transition: 0.4s;
-    text-decoration: none;
+  padding: 16px 24px;
+  border: none;
+  border-radius: 5px;
+  background: blue;
+  color: white;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 19px;
+  transition: 0.4s;
+  text-decoration: none;
 
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        font-size: 10px;
-        padding: 12px 20px;
-        line-height: 15px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 10px;
+    padding: 12px 20px;
+    line-height: 15px;
+  }
 
-    &:hover{
-        filter: brightness(115%);
-    }
+  &:hover {
+    filter: brightness(115%);
+  }
 
-    &:active{
-        filter: brightness(150%);
-    }
+  &:active {
+    filter: brightness(150%);
+  }
 `;
